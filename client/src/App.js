0a8a3c0c2c33ed5be4bbf3a8d0e17b4ea5812f07
Zriyaz <mdriyazuddine17@gmail.com>
2021-01-03 02:41:06 -0800

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,6 @@ const App = () => {
           <Route path='/search/:keyword' component={HomeScreen} exact />
           <Route path='/page/:pageNumber' component={HomeScreen} exact />
           <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
-
         </Container>
       </main>
       <Footer />
